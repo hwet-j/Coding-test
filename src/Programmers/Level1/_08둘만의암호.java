8ca53316ba6778d28e5177f5595f62696fe5f5b6
Hwet <ghlckd5424@gmail.com>
2023-05-26 15:54:15 +0900

    둘만의암호

---
+++ src/Programmers/Level1/_08둘만의암호.java
@@ -0,0 +1,14 @@
+package src.Programmers.Level1;
+
+public class _08둘만의암호 {
+
+	public static void main(String[] args) {
+		String s = "aukks";
+		String skip = "wbqd";
+		int index = 5;
+
+
+
+	}
+}
+
