691047eb535b850cb850482d8f7b3e955dab6b5a
hwet-j <ghlckd5424@gmail.com>
2023-06-04 15:37:17 +0900

    Level 0 풀이완료

--- src/Programmers/Level0/_86.java
+++ src/Programmers/Level0/_86.java
@@ -2,12 +2,47 @@ package src.Programmers.Level0;
 
 
 /*
-
+양의 정수 n이 매개변수로 주어집니다. n × n 배열에 1부터 n2 까지 정수를 인덱스
+[0][0]부터 시계방향 나선형으로 배치한 이차원 배열을
+return 하는 solution 함수를 작성해 주세요.
 */
+
+import java.util.Arrays;
 
 public class _86 {
 	public static void main(String[] args) {
+		int n = 4;
+		int[][] answer = new int[n][n];
+		
+		// 입력값
+		int num = 1;
 
+		// 4개의 기준에 Line을 찾기위해 변수 설정
+		int cnt1 = 0;
+
+		// 입력값이 n제곱을 넘으면 정지
+		while(num < n*n+1) {
+			// 좌 -> 우
+			for (int i = cnt1; i < n - cnt1; i++) {
+				answer[cnt1][i] = num++;
+			}
+			// 상 -> 하
+			for (int i = cnt1 + 1; i < n - cnt1; i++) {
+				answer[i][n - (cnt1 + 1)] = num++;
+			}
+			// 우 -> 좌
+			for (int i = n - (cnt1 + 2); i >= cnt1; i--) {
+				answer[n - (cnt1 + 1)][i] = num++;
+			}
+			// 하 -> 상
+			for (int i = n - (cnt1 + 2); i > cnt1; i--) {
+				answer[i][cnt1] = num++;
+			}
+			cnt1++;		// 1회 회전 완료
+		}
+		for (int[] ans : answer) {
+			System.out.println(Arrays.toString(ans));
+		}
 
 	}
 }
---
+++ src/Programmers/Level0/_88.java
@@ -0,0 +1,31 @@
+package src.Programmers.Level0;
+
+
+/*
+이차원 정수 배열 arr이 매개변수로 주어집니다. arr의 행의 수가 더
+많다면 열의 수가 행의 수와 같아지도록 각 행의 끝에 0을 추가하고,
+열의 수가 더 많다면 행의 수가 열의 수와 같아지도록 각 열의 끝에 0을 추가한
+이차원 배열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+
+public class _88 {
+	public static void main(String[] args) {
+		int[][] arr = {{572, 22, 37}, {287, 726, 384}, {85, 137, 292}, {487, 13, 876}};
+		int width = arr[0].length;
+		int height = arr.length;
+
+		int setSize = (height > width ? height : width);
+
+		int[][] answer = new int[setSize][setSize];
+
+		for(int i=0;i<height; i++) {
+			for (int j=0;j<width; j++){
+				answer[i][j] = arr[i][j];
+			}
+		}
+
+	}
+}
+
+
