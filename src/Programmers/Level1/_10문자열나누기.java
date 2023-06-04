b63516bdfe7b87abc13f75797d00a440e26de9bd
hwet-j <ghlckd5424@gmail.com>
2023-06-04 15:37:29 +0900

    문자열나누기

--- src/Programmers/Level1/_10문자열나누기.java
+++ src/Programmers/Level1/_10문자열나누기.java
@@ -1,33 +1,34 @@
 package src.Programmers.Level1;
 
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+//https://school.programmers.co.kr/learn/courses/30/lessons/140108
 
 public class _10문자열나누기 {
 	public static void main(String[] args) {
-		String s = "banana";
-		Set<String> set = new HashSet();
-		for (int i=0; i<s.length();i++){
-			set.add(s.substring(i, i+1));
-		}
-		Map<String, Integer> map = new HashMap();
-		int cnt = 0;
-		for (String item:set){
-			map.put(item,0);
-		}
+		String s = "abracadabra";
+		int answer = 0;
+		int mainCnt = 0;
+		String mainStr = "";
+		int subCnt = 0;
 
-		for (int i=0;i<s.length();i++){
-			String key = s.substring(i, i+1);
-			map.replace(key, map.get(key) + 1);
-			for (String mapKey: map.keySet()){
-				if (key == s.substring(i, i+1));
-
+		for (int i=0; i<s.length(); i++){
+			if(mainCnt == 0){
+				mainStr = s.substring(i,i+1);
+				mainCnt++;
+			} else if (mainStr.equals(s.substring(i,i+1))){
+				mainCnt++;
+			} else {
+				subCnt++;
 			}
-
+			if (mainCnt == subCnt){
+				mainCnt = 0;
+				subCnt = 0;
+				answer++;
+			} else if (i == s.length()-1){
+				answer++;
+			}
 		}
+
+
 
 
 	}
