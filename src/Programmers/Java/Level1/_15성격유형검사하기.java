76955a0201ce7410f3f7286e04a552a834c8af82
hwet <ghlckd5424@gmail.com>
2023-06-07 18:00:37 +0900

    성격유형검하기

---
+++ src/Programmers/Java/Level1/_15성격유형검사하기.java
@@ -0,0 +1,13 @@
+package src.Programmers.Java.Level1;
+
+
+public class _15성격유형검사하기 {
+    public static void main(String[] args){
+        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+        int[] choices = {5, 3, 2, 7, 5};
+
+    }
+}
+
+
+
