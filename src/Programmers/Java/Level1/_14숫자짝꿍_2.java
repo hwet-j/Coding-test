b879f6f9007eef3795f2e30066fd24115e6c4e66
hwet <ghlckd5424@gmail.com>
2023-06-07 17:24:02 +0900

    숫자짝꿍

--- src/Programmers/Java/Level1/_14숫자짝꿍_2.java
+++ src/Programmers/Java/Level1/_14숫자짝꿍_2.java
@@ -11,61 +11,63 @@ public class _14숫자짝꿍_2 {
     public static void main(String[] args){
         String X = "5525";
         String Y = "1255";
+        
+        // String타입 대신 문자열을 연산하기위해 객체 선언
+        StringBuilder temp = new StringBuilder();
 
-        StringBuffer answer = new StringBuffer("");
-
+        // 길이
         int xleng = X.length();
         int yleng = Y.length();
         // X의 0~9까지 각 개수
-        Map<Character, Integer> map = new HashMap<>();
+        int[] xvalue = new int[10];
         // Y의 0~9까지 각 개수
-        Map<Character, Integer> map2 = new HashMap<>();
+        int[] yvalue = new int[10];
+        // X,Y의 짝꿍 개수
+        int xymate = 0;
 
-        Set<Character> set = new HashSet<>();
-        
-        // X의 유니크값 가져오기
-        for(int i=xleng-1; i>=0; i--){
-            set.add(X.charAt(i));
-        }
-        int xcnt;
-        int ycnt;
-        for(char s:set){
-            xcnt = 0;
-            ycnt = 0;
-            for (int i=xleng-1; i>=0; i--){
-                if (X.charAt(i)==s){
-                    xcnt++;
+        // X, Y의 유니크값 가져오기
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < xleng; i++) {
+                if (Character.getNumericValue(X.charAt(i)) == j) {
+                    xvalue[j] += 1;
                 }
             }
-            for (int i=yleng-1; i>=0; i--){
-                if (Y.charAt(i)==s){
-                    ycnt++;
-                }
-            }
-            map.put(s,xcnt);
-            map2.put(s,ycnt);
-        }
-
-        for (char c:set){
-            if(map.get(c) > map2.get(c)){
-                for (int i=map2.get(c); i>0; i--){
-                    answer.insert(0,c);
-                }
-            } else {
-                for (int i=map.get(c); i>0; i--){
-                    answer.insert(0,c);
+            for (int i = 0; i < yleng; i++) {
+                if (Character.getNumericValue(Y.charAt(i)) == j) {
+                    yvalue[j] += 1;
                 }
             }
         }
-        String result = "";
-        if(answer.toString().equals("")){
-            result = "-1";
+        // 앞에서부터 큰수가 작성되어야 하기 떄문에 9 -> 0순으로 진행
+        for (int j = 9; j >= 0; j--) {
+            // X,Y의 짝꿍 개수
+            xymate = Math.min(xvalue[j], yvalue[j]);
+            for (int i = 0; i < xymate; i++) {
+                temp.append(j);
+            }
+        }
+        String answer = "";
+        if (temp.toString().equals("")){    // 비어있으면 짝궁이없음
+            answer = "-1";
         } else {
-            BigInteger num = new BigInteger(answer.toString());
-            result = "" + num;
+            answer = temp.toString();
+            if(answer.substring(0,1).equals("0")){  // 가장앞숫자가 0이면 0
+                answer = "0";
+            }
         }
-        System.out.println(result);
+        System.out.println(answer);
+
     }
 }
 
 
+/*
+* 시간초과 때문에 애먹은 문제
+* for문에서 조건에 x.length()와같은 메서드를 사용하면 for문이 1회 진행될때마다 메서드가 호출되기 때문에 지양하는게 좋다.
+* x.length()를 사용하고 싶다면 시작점으로 설정하고 하나씩 빼는 형식으로 작성한다.
+* 아니면 x.length()를 변수에 저장하여 저장된 변수를 사용한다.
+* String보다 Character가 더욱 가볍다. (메모리할당량이 낮다)
+* String타입에서 + 연산을하면 + 연산을 할때마다 새로운 문자열을 생성하고 할당하는 작업을 한다.
+* 그렇기 때문에 매우 무거워질수있다. 이때 StringBuilder 또는 StringBuffer를 사용하면 좋다. (가변객체)
+* 또한, 숫자 계산을 위해서 Biginteger를 사용했었는데 매우 무거운 객체인듯하다. 무슨짓을해도 시간초과가 뜬다.
+* */
