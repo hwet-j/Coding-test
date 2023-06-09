4d230c2efbb0cb17ddd7ae371ed82aca93914dc5
hwet <ghlckd5424@gmail.com>
2023-06-09 17:33:04 +0900

    IN

---
+++ src/Programmers/OracleDB/Level2/_20루시와 엘라 찾기.sql
@@ -0,0 +1,11 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ( 'Lucy', 'Ella','Pickle', 'Rogan', 'Sabrina','Mitty')
+ORDER BY ANIMAL_ID
+
+
+
+/*
+문제의 조건이 예시에 나와있는 이상한 문제
+정답처리가 안된다면 예시에 조건이 나와있으니 확인
+*/
