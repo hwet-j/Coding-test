a07d304dd440bd7121917781ac600343cf32ffbc
hwet <ghlckd5424@gmail.com>
2023-06-13 10:29:19 +0900

    숫자문자열과영단어

---
+++ src/Programmers/Java/Level1/_17숫자문자열과영단어.java
@@ -0,0 +1,28 @@
+package src.Programmers.Java.Level1;
+
+
+import java.util.*;
+
+public class _17숫자문자열과영단어 {
+    public static void main(String[] args){
+        String s = "one4seveneight";
+
+        s = s.replace("zero", "0");
+        s = s.replace("one", "1");
+        s = s.replace("two", "2");
+        s = s.replace("three", "3");
+        s = s.replace("four", "4");
+        s = s.replace("five", "5");
+        s = s.replace("six", "6");
+        s = s.replace("seven", "7");
+        s = s.replace("eight", "8");
+        s = s.replace("nine", "9");
+
+        int answer = Integer.parseInt(s);
+
+        System.out.println(s);
+    }
+}
+
+
+
