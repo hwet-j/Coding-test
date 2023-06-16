3d941a2755fca7343d99e3cbf87be302258f37ea
hwet <ghlckd5424@gmail.com>
2023-06-16 21:27:58 +0900

    실패율

---
+++ src/Programmers/Java/Level1/_22실패율.java
@@ -0,0 +1,20 @@
+package src.Programmers.Java.Level1;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _22실패율 {
+    public static void main(String[] args) {
+        int N = 5;
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+
+        int people = stages.length;
+        int temp = 0;
+        int left_pp = 0;
+        int cnt = 0;
+        int[] result = new int[N];
+
+    }
+}
+
