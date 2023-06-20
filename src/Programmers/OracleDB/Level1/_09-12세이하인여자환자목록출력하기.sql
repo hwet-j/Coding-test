aab5463e524647cf34bc8d804791d6c95d4c1d5d
hwet <ghlckd5424@gmail.com>
2023-06-20 17:18:16 +0900

    12세이하인여자환자목록출력

--- src/Programmers/OracleDB/Level1/_09-12세이하인여자환자목록출력하기.sql
+++ src/Programmers/OracleDB/Level1/_09-12세이하인여자환자목록출력하기.sql
@@ -5,4 +5,11 @@ ORDER BY AGE DESC, PT_NAME ASC
 
 /*
 NVL(컬럼, 대체값) - NULL 값인지 확인하여 NULL값인 경우 대체값으로 수정해준다.
+
+NULL 값을 다루는 함수는 NVL, NVL2, COALESCE 가 있는데
+
+가장 간단한 NVL을 사용하여 변경
+
+NVL은 표현식이 NULL이면 두 번째 인자로 값을 수정한다.
+
 */
