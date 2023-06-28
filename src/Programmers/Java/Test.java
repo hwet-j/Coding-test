0e48259bbc3f0988d69f5db367e240fcc45bb502
hwet <ghlckd5424@gmail.com>
2023-06-28 17:55:18 +0900

    피로도 풀이중

--- src/Programmers/Java/Test.java
+++ src/Programmers/Java/Test.java
@@ -6,22 +6,30 @@ import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args){
-        double[] array = {0.001095, 5.1E-1,  0.591914, 5.1E-6};
+        int[] arr = {1,21,3,5,7,21};
 
-        // Double[]로 변환
-        Double[] boxedArray = Arrays.stream(array).boxed().toArray(Double[]::new);
+        int maxindex = findMaxIndex(arr);
+        System.out.println(Arrays.stream(arr).max());
+        System.out.println(maxindex);
 
-        // 내림차순으로 정렬
-        Arrays.sort(boxedArray, Comparator.reverseOrder());
 
-        // 정렬된 배열 출력
-        System.out.println(Arrays.toString(boxedArray));
+    }
 
-        if (array[3] >= array[0]){
-            System.out.println("ㅇㅇ?");
-        } else if  (array[3] < array[0]){
-            System.out.println("nn");
+    public static int findMaxIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("배열이 비어 있습니다.");
         }
 
+        int maxIndex = 0;
+        int maxValue = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
     }
 }
